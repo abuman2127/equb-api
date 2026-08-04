d56c5469c9a5b2ba0fabfc0dd0617582395457c2
Amir <amirshemsu544@gmail.com>
2026-08-04 23:12:50 +0300

    Enable SSL for Neon

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 // ---- Auth middleware ----
